f04c87475d755856b1bb6763c98d0da07b0b8d54
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-02-11 11:33:58 -0300

    faet: define methods

--- SimpleSort/header.h
+++ SimpleSort/header.h
@@ -1,4 +1,8 @@
 #ifndef SIMPLE_SORT
 #define SIMPLE_SORT
 
+void bubble(int* vector, int size);
+void selection(int* vector, int size);
+void insertion(int* vector, int size);
+
 #endif // !SIMPLE_SORT
