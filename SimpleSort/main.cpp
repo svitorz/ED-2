1ac9cd6c17508ddae63e08c73e6c26b816eb7e95
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-02-11 11:34:07 -0300

    feat: create an interface to sortings

--- SimpleSort/main.cpp
+++ SimpleSort/main.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
+#include <cctype>
 #include "header.h"
 
 using namespace std;
 
 int main(){
+  int length, i;
+
+  cout << "Insira o tamanho do vetor:";
+  cin >> length;
+
+  int vector[length];
+
+  for (i = 0; i < length; i++) {
+    cout << "\nInsira um valor inteiro para a posicao " << (i + 1) << " do vetor:";
+    cin >> vector[i];
+  }
+
+  cout << "\nVetor desordenado.\n";
+  for (i = 0; i < length; i++) {
+    std::cout << vector[i] << ". "; 
+  }
+
+  char opc;
+  cout << "\nInsira o metodo de ordenacao:\n"
+          "b - bubble\n"
+          "i - insertion\n"
+          "s - selection:";
+
+  cin >> opc;
+
+  switch (tolower(opc)) {
+    case 'b':
+      bubble(vector, length);
+      break;
+    case 'i':
+      insertion(vector, length);
+      break;
+    case 's':
+      selection(vector, length);
+      break;
+    default:
+      break;
+      return 0;
+  }
+
+  cout << "\nVetor ordenado.\n";
+  for (i = 0; i < length; i++) {
+    cout << vector[i] << ". "; 
+  }
+
   return 0;
 }
